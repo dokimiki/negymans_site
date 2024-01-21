e2532e4463063ad35a65f20eac04b83bce69313d
どきみき <lmznd6yiew@gmail.com>
2024-01-22 01:35:48 +0900

    Fix environment configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = {
     reactStrictMode: true,
     swcMinify: true,
     output: 'export',
-    ...require(`./config/${process.env.APP_ENV} || 'default'}.json`),
+    ...require(`./config/${process.env.APP_ENV || 'default'}.json`),
 };
