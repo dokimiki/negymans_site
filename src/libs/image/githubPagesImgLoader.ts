d1d1ba47b42ae40749de63b85e4e7f8f3d3422d5
どきみき <lmznd6yiew@gmail.com>
2024-01-22 03:22:36 +0900

    Update image loader path in github.json

--- src/libs/image/githubPagesImgLoader.ts
+++ src/libs/image/githubPagesImgLoader.ts
@@ -1,6 +1,6 @@
 export default function imageLoader({ src, width, quality }: { src: string; width: number; quality?: number }): string {
     if (src.startsWith("/")) {
-        return `/${process.env.NEXT_PUBLIC_BASE_PATH}${src}`;
+        return `/negymans_site${src}`;
     }
     return src;
 }
