ff1c126a4cb316c740dbf9554f91ad2c9d647050
どきみき <lmznd6yiew@gmail.com>
2024-01-22 03:25:03 +0900

    Remove unused image loader and update GitHub configuration

--- src/libs/image/githubPagesImgLoader.ts
+++
@@ -1,6 +0,0 @@
-export default function imageLoader({ src, width, quality }: { src: string; width: number; quality?: number }): string {
-    if (src.startsWith("/")) {
-        return `/negymans_site${src}`;
-    }
-    return src;
-}
