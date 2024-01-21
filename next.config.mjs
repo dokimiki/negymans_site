5c5498527c229c99cb306e40f59aefc65927715f
どきみき <lmznd6yiew@gmail.com>
2024-01-22 01:31:26 +0900

    Add Next.js configuration and update TypeScript and GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', };
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'export',
+    ...require(`./config/${process.env.APP_ENV} || 'default'}.json`),
+};
 
 export default nextConfig;
