84388445b0c4c870b2fcfdfac888b64e0a0a74da
どきみき <lmznd6yiew@gmail.com>
2024-01-22 02:06:57 +0900

    Commented out swcMinify in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     reactStrictMode: true,
-    swcMinify: true,
+    //swcMinify: true,
     output: 'export',
     ...require(`./config/${process.env.APP_ENV || 'default'}.json`),
 };
