a4b604ed30e2bcba66526450920d7015471d8c7b
どきみき <lmznd6yiew@gmail.com>
2024-03-06 00:53:09 +0900

    scssを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import "./global.scss";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          this is header
+        </header>
+        {children}
+        <div className="h-screen">this is footer</div>
+        <footer></footer>
+      </body>
     </html>
   );
 }
