033b86bb8486599f77d78c7e0e3b243a1e746a00
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2024-03-06 22:57:11 +0900

    wip

--- src/app/News.tsx
+++ src/app/News.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import "./News.scss";
 
 export default function News() {
+    const newsList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const news = newsList.map((n) => <li key={n}>{n}</li>);
     return (
         <>
             <h2>News</h2>
-            <p>Here are some news</p>
+            <h1>お知らせ</h1>
+            <ul>{news}</ul>
         </>
     );
 }
