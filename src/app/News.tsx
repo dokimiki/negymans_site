a0aab56af6beae96c49808ac3acbdaca4067d860
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2024-03-07 01:57:48 +0900

    wip

--- src/app/News.tsx
+++ src/app/News.tsx
@@ -21,12 +21,19 @@ export default function News() {
 function newsList() {
     const newsList: NewsObject[] = [
         {
-            title: "アリーナ発表",
-            explanation: "先日アリーナで発表会",
-            date: "2023/3/11",
+            title: "NEGYMANSのWebサイトを公開",
+            explanation:
+                "Stet amet ipsum amet diam lorem nulla sit tempor. Molestie suscipit takimata duis sed est elitr duis nulla sit diam stet ipsum et justo suscipit. Lorem vel takimata aliquyam justo et kasd dolor nonummy. Stet invidunt elitr ut dolores exerci ipsum nibh elitr dolor dolor praesent. Magna dignissim rebum voluptua ipsum sit dolor nulla at lorem doming vero tempor no et clita. Amet tempor diam sit aliquyam stet sanctus et gubergren iusto sed sed at amet nibh consequat. Sanctus sadipscing nostrud at feugiat imperdiet illum dolore tempor enim. Kasd feugiat erat wisi erat. Sit lorem vero no velit et facilisis kasd eirmod dolor dolor ",
+            date: "2023.3.1",
             url: "https://www.negymans.com/",
         },
-        { title: "GOTSIMULATOR", explanation: "ゲームを作ったよ", date: "2023/3/44", url: "https://www.negymans.com/" },
+        {
+            title: "アリーナ発表",
+            explanation: "先日アリーナで発表会",
+            date: "2023.3.11",
+            url: "https://www.negymans.com/",
+        },
+        { title: "GOTSIMULATOR", explanation: "ゲームを作ったよ", date: "2023.3.44", url: "https://www.negymans.com/" },
     ];
     return (
         <>
