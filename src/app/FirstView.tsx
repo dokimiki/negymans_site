50040e4b100323dba6e0de3399e66a5d2f0bd5bf
kinako1415 <85997461+kinako1415@users.noreply.github.com>
2024-03-07 16:46:07 +0900

    FirstViewの作成途中

--- src/app/FirstView.tsx
+++ src/app/FirstView.tsx
@@ -1,8 +1,11 @@
+import React from "react";
+import "./FirstView.scss";
+
 export default function FirstView() {
     return (
         <div className="catchcopy">
-            <div className="text-facing">subcatchcopy</div>
-            <div className="text-blue-bg">catchcopy</div>
+            <div className="text-facing sub-catchcopy">subcatchcopy</div>
+            <div className="text-blue-bg main-catchcopy">キャッチコピー</div>
             <div className="text-underline">catchcopycommentary</div>
         </div>
     );
