a2ca958d4fb780323188af4e4f95e5460242e35f
kinako1415 <85997461+kinako1415@users.noreply.github.com>
2024-03-08 15:07:03 +0900

    キャッチコピーの変更

--- src/app/FirstView.tsx
+++ src/app/FirstView.tsx
@@ -5,7 +5,7 @@ export default function FirstView() {
     return (
         <div className="catchcopy">
             <div className="text-facing sub-catchcopy">Explore new worlds</div>
-            <div className="text-blue-bg main-catchcopy">新たな世界に挑め</div>
+            <div className="text-blue-bg main-catchcopy">新たな世界が君を呼ぶ</div>
             <div className="detail-catchcopy">
                 catchcopycommentarycatchcopycommentary<br/>
                 catchcopycommentarycatchcopyco
