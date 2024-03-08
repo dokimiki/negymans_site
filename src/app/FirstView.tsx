54031216ba7e50f7e2c82feef113585a21547b55
kinako1415 <85997461+kinako1415@users.noreply.github.com>
2024-03-08 14:22:06 +0900

    FirstViewの完成

--- src/app/FirstView.tsx
+++ src/app/FirstView.tsx
@@ -5,8 +5,11 @@ export default function FirstView() {
     return (
         <div className="catchcopy">
             <div className="text-facing sub-catchcopy">subcatchcopy</div>
-            <div className="text-blue-bg main-catchcopy">キャッチコピー</div>
-            <div className="text-underline">catchcopycommentary</div>
+            <div className="text-blue-bg main-catchcopy">新たな世界に挑め</div>
+            <div className="detail-catchcopy">
+                catchcopycommentarycatchcopycommentary<br/>
+                catchcopycommentarycatchcopyco
+            </div>
         </div>
     );
 }
