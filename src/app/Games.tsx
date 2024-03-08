6e22177d2e818fad6e0f7ae170fd1c8d3c36fcf2
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2024-03-08 21:05:15 +0900

    wip

--- src/app/Games.tsx
+++ src/app/Games.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
+import "./Games.scss";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
-import miku from "..231x87.png";
 
 type gameObject = {
     gameTitle: string;
@@ -20,7 +20,6 @@ export default function Games() {
                 <h2 className="text-blue-bg">作ったゲーム</h2>
             </div>
             {GamesList()}
-            {s()}
         </div>
     );
 }
@@ -32,66 +31,47 @@ function GamesList() {
             explanation:
                 "Stet amet ipsum amet diam lorem nulla sit tempor. Molestie suscipit takimata duis sed est elitr duis nulla sit diam stet ipsum et justo suscipit. Lorem vel takimata aliquyam justo et kasd dolor nonummy. Stet invidunt elitr ut dolores exerci ipsum nibh elitr dolor dolor praesent. Magna dignissim rebum voluptua ipsum sit dolor nulla at lorem doming vero tempor no et clita. Amet tempor diam sit aliquyam stet sanctus et gubergren iusto sed sed at amet nibh consequat. Sanctus sadipscing nostrud at feugiat imperdiet illum dolore tempor enim. Kasd feugiat erat wisi erat. Sit lorem vero no velit et facilisis kasd eirmod dolor dolor ",
             gameUrl: "https://www.negymans.com/",
-            gameImage: "画像はイメージです",
+            gameImage: "https://placehold.jp/1920x1080.png",
         },
         {
             gameTitle: "Apex Legends",
             explanation: "すばらしいゲーム",
             gameUrl: "https://www.negymans.com/",
-            gameImage: "画像はイメージです",
+            gameImage: "https://placehold.jp/1920x1080.png",
         },
         {
             gameTitle: "松下の分岐型ゲーム",
             explanation: "ゲームを作ったよ",
             gameUrl: "https://www.negymans.com/",
-            gameImage: "画像はイメージです",
+            gameImage: "https://placehold.jp/1920x1080.png",
         },
     ];
     return (
         <>
             <ul>
-                <h1>{gameList[0].gameTitle}</h1>
-                {gameList.map((n) => (
-                    <li key={1}>
-                        <p>{n.gameImage}</p>
-                        <h3>{n.gameTitle}</h3>
-
-                        <p>{n.explanation}</p>
-
-                        <a href={n.gameUrl}></a>
-                    </li>
-                ))}
+                <Splide
+                    options={{
+                        perMove: 1,
+                        gap: 1,
+                        cover: true,
+                        heightRatio: 0.5,
+                        updateOnMove: true,
+                        padding: "15rem",
+                        type: "loop",
+                        focus: "center",
+                        width: "100%",
+                    }}
+                    aria-label="React Splide Example"
+                >
+                    {gameList.map((n) => (
+                        <SplideSlide key={1}>
+                            <picture>
+                                <img className="game-img" src={n.gameImage} alt="placeholder" />
+                            </picture>
+                        </SplideSlide>
+                    ))}
+                </Splide>
             </ul>
         </>
     );
 }
-
-export function s() {
-    return (
-        <Splide
-            options={{
-                perMove: 1,
-                gap: 16,
-                cover: true,
-                heightRatio: 0.5,
-                updateOnMove: true,
-                padding: "5rem",
-                type: "loop",
-                focus: "center",
-            }}
-            aria-label="React Splide Example"
-        >
-            <SplideSlide>
-                <picture>
-                    <img src="../img/231x87.png" alt="placeholder" />
-                </picture>
-            </SplideSlide>
-            <SplideSlide>
-                <p>test2</p>
-            </SplideSlide>
-        </Splide>
-    );
-}
-
-//const splide = new Splide(".splide", options);
-//splide.mount();
