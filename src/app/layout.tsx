757389e0e42e275cc12762c0b8fb28d651c47c9c
Daiki Matsushita(松下大暉) <85777234+Matsudai-bit@users.noreply.github.com>
2024-03-11 05:36:54 +0900

    responsive yeah

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,38 +5,48 @@ import "./global.scss";
 import Image from "next/image";
 
 const notoSansJP = Noto_Sans_JP({
-  subsets: ["latin"],
-  variable: '--font-noto-sans-jp',
+    subsets: ["latin"],
+    variable: "--font-noto-sans-jp",
 });
 
 export const metadata: Metadata = {
-  title: "NEGYMANS",
-  description: "negymans site",
+    title: "NEGYMANS",
+    description: "negymans site",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body className={notoSansJP.className}>
-        <header className="glass-bg sticky inset-0 flex flex-row justify-between">
-          <div><Image className="negymans-logo" src="/img/NEGYMANS.svg" alt="NEGYMANS" width={270} height={270} /></div>
-          <div className="flex flex-row">
-            <nav>
-              <ul className="header-right-content flex flex-row">
-                <li><a className="link-button transparent-button negymans-button" href=""><p>NEGYMANSとは</p></a></li>
-                <li><a className="link-button transparent-button contents-button" href=""><p>Contents</p></a></li>
-              </ul>
-            </nav>
-            <div>三</div>
-          </div>
-        </header>
-        {children}
-        <footer></footer>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="ja">
+            <body className={notoSansJP.className}>
+                <header className="glass-bg sticky inset-0 flex flex-row justify-between">
+                    <div>
+                        <Image className="negymans-logo" src="/img/NEGYMANS.svg" alt="NEGYMANS" width={270} height={270} />
+                    </div>
+                    {/* <div className="flex flex-row">
+                        <nav>
+                            <ul className="header-right-content flex flex-row">
+                                <li>
+                                    <a className="link-button transparent-button negymans-button" href="">
+                                        <p>NEGYMANSとは</p>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="link-button transparent-button contents-button" href="">
+                                        <p>Contents</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <div>三</div>
+    </div>*/}
+                </header>
+                {children}
+                <footer></footer>
+            </body>
+        </html>
+    );
 }
